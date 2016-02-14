409848bce8284b3eb8776b76a9a9878d357bc579
John Cossack <Greatmadisonavenue@gmail.com>
2016-02-13 18:55:19 -0800

    added tutorial

---
+++ tutorial/segan.rb
@@ -0,0 +1,17 @@
+def sagan
+  puts "Somewhere something incredible is waiting to be known"
+end
+
+sagan
+puts "I like to move it, move it"
+$end
+
+
+def osteen
+  puts "You can change your world by changing your words"
+  
+end
+ 
+osteen
+print "The watermelon crawl"
+$end
