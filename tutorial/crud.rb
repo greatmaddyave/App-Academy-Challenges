d6fee00a3c654b03411e4e592c1f12147f7d6a93
John Cossack <Greatmadisonavenue@gmail.com>
2016-02-15 12:43:21 -0800

    Lesson 24

--- tutorial/crud.rb
+++ tutorial/crud.rb
@@ -39,6 +39,22 @@ puts "What would you like to do? Options: (add) (update) (delete) (list)"
             books[title.to_sym] = rating.to_i
             puts "Your rating for #{title} has been updated to #{rating}"
           end
-          
+    when "delete"
+        puts "What book would you like to remove from your list"
+        title = gets.chomp.to_sym
+          if books[title.to_sym] .nil?
+            puts "#{title} has not been found in your list"
+          else
+            books.delete(title)
+            puts "#{title} has been deleted from your list"
+          end
+    when "list"
+        books.each do |book, rating|
+          puts "#{book}: #{rating}"
     end
+    
+  else
+    puts "Error, you've broken something :("
+  end
+          
    puts books
