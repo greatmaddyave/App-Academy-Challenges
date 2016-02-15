47a9f37dd3e2b72c1cabd79df4fd2cfd791389f4
John Cossack <Greatmadisonavenue@gmail.com>
2016-02-15 12:31:11 -0800

    Lesson 23

---
+++ tutorial/crud.rb
@@ -0,0 +1,44 @@
+#CRUD
+
+#creat
+#read
+#update
+#delete
+
+books = {
+  Wheels_of_Time: 4,
+  Mistborn: 5,
+  Alchamist: 7,
+}
+
+puts "What would you like to do? Options: (add) (update) (delete) (list)"
+   choice = gets.chomp
+   
+   case choice
+     
+     when "add"
+        puts "What book would you like to add?"
+        title = gets.chomp
+          if books[title.to_sym] .nil?    
+        puts "What is your rating for #{title} out of 10?"
+        rating = gets.chomp
+        books[title.to_sym] = rating.to_i
+        puts "#{title} has been added to your list with a rating of #{rating}"
+          else
+            puts "Book is already in list"
+          end
+      
+    when "update"
+        puts "What book would you like to update in your list?"
+        title = gets.chomp
+          if books[title.to_sym] .nil?
+            puts "#{title} has not been found in your current list"
+          else
+            puts "What new rating would you give #{title}"
+            rating = gets.chomp
+            books[title.to_sym] = rating.to_i
+            puts "Your rating for #{title} has been updated to #{rating}"
+          end
+          
+    end
+   puts books
